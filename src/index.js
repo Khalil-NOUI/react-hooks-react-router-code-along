f75757ab5a18ee4bf03b7d77bd2ffdbb9b264751
Khalil-NOUI <NOUI.KHALIL.PR@GMAIL.COM>
2023-10-12 04:59:48 +0100

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {BrowserRouter, Route, Switch, NavLink} from "react-router-dom"
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import NavBar from "./pages/NavBar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
+root.render(
+    <BrowserRouter>
+    <NavBar />
+    <Switch>
+      <Route exact path="/about">
+        <About />
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </BrowserRouter>,
+  );
